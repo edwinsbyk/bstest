7a57b492fb648a35a0b57a8d22512089fba3f4c5
Edwin Sumbayak <edwinsumbayak01@gmail.com>
2019-04-24 03:30:57 +0200

    ganteng

---
+++ profilku.php
@@ -0,0 +1,66 @@
+
+	<h1 class="content teks-tengah">Profilku</h1>
+	<div class="row">
+	<center>
+		<table class="table table-bordered table-striped">
+			<tbody>
+			<tr>
+				<td><b>Nama Lengkap</b></td>
+				<td><b>:</b></td>
+				<td> Edwin E Sumbayak</td>
+			</tr>
+			<tr>
+				<td><b>Nama Panggilan</b></td>
+				<td></b>	:</b> </td>
+				<td> Edwin</td>
+			</tr>
+			<tr>
+				<td><b>Tempat,Tanggal Lahir</b></td>
+				<td> :</b></td>
+				<td> PEmatang Raya, 29 Januari 2002</td>
+			</tr>
+			<tr>
+				<td><b>Umur</b></td>
+				<td> :</b> </td>
+				<td> 17 Tahun</td>
+			</tr>
+			<tr>
+				<td><b>Jenis Kelamin</b>
+				<td><b></b>:</b></td>
+				<td> Laki Laki</td>
+			</tr>
+			<tr>
+				<td><b>Gol. Darah</b>
+				<td><b>:</b></td>
+				<td> O</td>
+			</tr>
+			<tr>
+				<td><b>Agama</b></td>
+				<td><b>:</b></td>
+				<td> Kristen Protestan</td>
+			</tr>
+			<tr>
+				<td><b>Alamat</b></td>
+				<td><b>:</b></td>
+				<td> Jalan Jamin Ginting Gg. Kenanga VI</td>
+			</tr>
+			<tr>
+				<td><b>Status</b></td>
+				<td><b>:</b></td> 
+				<td> Pelajar</td>
+			</tr>
+			<tr>
+				<td><b>Pekerjaan</b></td>
+				<td><b> :</b></td>
+				<td> Pelajar</td>
+			</tr>
+			<tr>
+				<td><b>Kewarganegaraan</b></td>
+				<td><b>:</b></td>
+				<td> Indonesia</td>
+			</tr>
+			</tbody>
+		</table>
+		</center>
+	</div>
+
